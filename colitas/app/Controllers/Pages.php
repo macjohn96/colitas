13a5a76d0fe5e53a8325c740c0fbeb5acaa0c64f
John Carranza <john6996@hotmail.es>
2022-02-19 20:52:04 -0500

    Inicio del proyecto

---
+++ colitas/app/Controllers/Pages.php
@@ -0,0 +1,21 @@
+<?php namespace App\Controllers;
+use CodeIgniter\Controller;
+
+class Pages extends Controller{
+
+    public function index(){
+        return view('welcome_message');
+    }
+
+    public function view($page = 'home'){
+        if (!is_file(APPPATH.'/Views/'.$page.'.php')){
+            throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
+        }
+
+        $data['title'] = ucfirst($page);
+
+        echo view('header', $data);
+        echo view('/'.$page, $data);
+        echo view('footer', $data);
+    }
+}
---
+++ colitas/app/Views/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Tutorial</title>
+    </head>
+    <body>
+        <h1>Texto aleatorio</h1>
+        <h2><?= esc($title); ?></h1>
